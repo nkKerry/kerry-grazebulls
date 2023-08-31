a1b06078f878e372d807c9217719bde88bf509d2
Kerry <126693137+nkKerry@users.noreply.github.com>
2023-08-31 08:05:45 +0300

    Update client.lua
    added notifications: about the remaining time to graze,
    if the bull is killed and you try to interact with it.
    added the ability to stop the bull (stops following, uses animation)
    work mark shifted

--- kerry-grazebulls/client/client.lua
+++ kerry-grazebulls/client/client.lua
@@ -3,7 +3,7 @@ local pasturingBulls = {}
 local starting = false
 local isPasturing = false
 local pasturingStartTime = 0
-
+local isFinish = false
 
 Citizen.CreateThread(function()
     for bullmenu, v in pairs(Config.Bull) do
@@ -79,14 +79,15 @@ AddEventHandler('kerry-grazebulls:client:bullspawn', function()
             local randomY = playerCoords.y + math.random(-5, 5) -- Примерный диапазон по Y
 
             bull = CreatePed(hash, randomX, randomY, playerCoords.z, true, true, false)
+            Citizen.InvokeNative(0x23f74c2fda6e7c61, -1749618580, bull)
+            Citizen.InvokeNative(0x77FF8D35EEC6BBC4, bull, 0, false)
+			SetEntityAsMissionEntity(animal, true)
 		if follow then
 			local bullOffset = vector3(0.0, 2.0, 0.0) 
 			TaskFollowToOffsetOfEntity(bull, player, bullOffset.x, bullOffset.y, bullOffset.z, 1.0, -1, 0.0, 1)
 		end
-            Citizen.InvokeNative(0x23f74c2fda6e7c61, -1749618580, bull)
-            Citizen.InvokeNative(0x77FF8D35EEC6BBC4, bull, 0, false)
-
             table.insert(pasturingBulls, bull) 
+			Wait(500)
         end 
 		
         for _, bullId in ipairs(pasturingBulls) do
@@ -105,14 +106,22 @@ AddEventHandler('kerry-grazebulls:client:bullspawn', function()
                         label = Lang:t('menu.to_graze'),
 						targeticon = 'fas fa-solid fa-cow',
                         action = function()
-                            TriggerEvent('kerry-grazebulls:client:pas')
+                            TriggerEvent('kerry-grazebulls:client:pas', bullId)
+                        end,
+                    },
+					{
+                        type = 'client',
+                        label = Lang:t('menu.bull_stop'),
+						targeticon = 'fas fa-solid fa-cow',
+                        action = function()
+                            TriggerEvent('kerry-grazebulls:client:startanim', bullId)
                         end,
                     }
                 },
                 distance = 3.5,
             })
-        end
-       
+		end
+		
         if randomChance <= Config.RandomChance then
             TriggerEvent('kerry-grazebulls:client:hunters')
         end
@@ -121,22 +130,52 @@ end)
 
 RegisterNetEvent('kerry-grazebulls:client:lead')
 AddEventHandler('kerry-grazebulls:client:lead', function(bull)
-    local player = PlayerPedId()
-    local playerCoords = GetEntityCoords(player)
-    local bullOffset = vector3(0.0, 2.0, 0.0) 
- 
-    TaskFollowToOffsetOfEntity(bull, player, bullOffset.x, bullOffset.y, bullOffset.z, 1.0, -1, 0.0, 1)
+if IsPedDeadOrDying(bull, true) then
+	RSGCore.Functions.Notify(Lang:t('error.dead_bulls'), 'error', 3000)
+return end
+		local player = PlayerPedId()
+		local playerCoords = GetEntityCoords(player)
+		local bullOffset = vector3(0.0, 2.0, 0.0) 
+		ClearPedTasks(bull)
+		TaskFollowToOffsetOfEntity(bull, player, bullOffset.x, bullOffset.y, bullOffset.z, 1.0, -1, 0.0, 1)
 end)
 
+RegisterNetEvent('kerry-grazebulls:client:startanim')
+AddEventHandler('kerry-grazebulls:client:startanim', function(animal)
+if IsPedDeadOrDying(animal, true) then
+	RSGCore.Functions.Notify(Lang:t('error.dead_bulls'), 'error', 3000)
+return end
+
+    if DoesEntityExist(animal) then
+		local availableScenarios = {
+			"WORLD_ANIMAL_BULL_GRAZING",
+			"WORLD_ANIMAL_BULL_RESTING",
+			"WORLD_ANIMAL_BULL_SLEEPING"
+		}
+		local randomScenario = availableScenarios[math.random(1, #availableScenarios)]
+		local anim = randomScenario
+		ClearPedTasks(animal)
+		TaskStartScenarioInPlace(animal, GetHashKey(anim), -1, true, false, false, false)
+	end
+end)
 
 RegisterNetEvent('kerry-grazebulls:client:pas')
 AddEventHandler('kerry-grazebulls:client:pas', function(animal)
-    local isInZone = false
-	    if isPasturing then
+	if IsPedDeadOrDying(animal, true) then
+		RSGCore.Functions.Notify(Lang:t('error.dead_bulls'), 'error', 3000)
+		return 
+	end
+	if isFinish then
+		RSGCore.Functions.Notify(Lang:t('success.bull_finish'), 'success', 3000)
+        return
+    end
+	if isPasturing then
 		RSGCore.Functions.Notify(Lang:t('error.bull_already'), 'error', 3000)
         return
     end
-    
+	
+	local isInZone = false
+	
     for _, bull in ipairs(pasturingBulls) do
         if DoesEntityExist(bull) then
             for _, zoneCoords in ipairs(Config.BullZone) do
@@ -175,7 +214,6 @@ AddEventHandler('kerry-grazebulls:client:pas', function(animal)
 	startPasturing()
     end
 end)
-
 
 function modelrequest( model )
     Citizen.CreateThread(function()
@@ -287,6 +325,7 @@ AddEventHandler('kerry-grazebulls:client:bullreturn', function()
                 RSGCore.Functions.Notify(Lang:t('error.dead_bull'), 'error', 3000)
                 end
                 isPasturing = false
+				isFinish = false
             end
         else
             RSGCore.Functions.Notify(Lang:t('error.error_time'), 'error', 3000)
@@ -298,16 +337,26 @@ end)
 
 function startPasturing()
     Citizen.CreateThread(function()
+	 local lastNotificationTime = 0
         while true do
-            Citizen.Wait(5000)
+            Citizen.Wait(10000)
             
             if isPasturing then
                 local currentTime = GetGameTimer()
                 local elapsedTime = currentTime - pasturingStartTime
+                local remainingTime = Config.WaitTimeBull - elapsedTime
                 
                 if elapsedTime >= Config.WaitTimeBull then
                     RSGCore.Functions.Notify(Lang:t('success.time_grazing'), 'success', 6000)
+					isFinish = true
                     break
+                end
+                
+                local remainingMinutes = math.floor(remainingTime / 60000) 
+                
+                if remainingMinutes > 0 and currentTime - lastNotificationTime >= 60000 then
+                    RSGCore.Functions.Notify(Lang:t('primary.minutes_left') .. remainingMinutes, 'primary', 6000)
+                    lastNotificationTime = currentTime
                 end
             end
         end
