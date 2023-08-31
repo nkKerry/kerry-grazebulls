b32e8b479fa7f08f0a85f6df65c4c6f3f7e8f670
Kerry <126693137+nkKerry@users.noreply.github.com>
2023-08-31 08:06:13 +0300

    Update en.lua

--- kerry-grazebulls/locales/en.lua
+++ kerry-grazebulls/locales/en.lua
@@ -2,6 +2,7 @@ local Translations = {
     error = {
 	  error_bull = 'You didn\'t herd bulls!',
 	  error_dead = 'You were killed by the hunters, you could not return the bulls!',
+	  dead_bulls = 'The bull is killed!',
 	  lead_ranch = 'Get away from the ranch!',
 	  job_error = 'You have already taken the job!',
 	  bull_already = 'The bulls are already grazing',
@@ -13,7 +14,8 @@ local Translations = {
     success = {
 	  time_grazing = 'You can return the bulls!',
       success_bull = 'Great job!',
-	  bull_grazing = 'The bull is grazing'
+	  bull_grazing = 'The bull is grazing',
+	  bull_finish = 'The bulls can be returned!'
     },
     menu = {
       job_bull = 'Take a job herding bulls',
@@ -21,6 +23,7 @@ local Translations = {
       job_shepherd = 'Shepherd',
       lead_menu = 'Lead',
 	  to_graze = 'To graze',
+	  bull_stop = 'Stop',
       close_menu = 'Close'
     }
 }
